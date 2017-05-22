76343dfb8261dcbf57e568e39c9570d3eff16fd6
emmpak <eiliev05@yahoo.com>
2017-05-22 13:25:41 +0100

    Add formatAirportStatus function

--- airport/test/test_faa_data_helper.js
+++ airport/test/test_faa_data_helper.js
@@ -65,8 +65,8 @@ describe('FAADataHelper', function() {
     });
     context('with a status containing a delay', function() {
       it('formats the status as expected', function() {
-        status.delay = true;
-        expect(subject.formatAirportStatus(status).to.eq('There is currently a delay for Hartsfield-Jackson Atlanta International. The average delay time is 57 minutes. Delay is because of the following: AIRLINE REQUESTED DUE TO DE-ICING AT AIRPORT / DAL AND DAL SUBS ONLY. The current weather conditions are Light Snow, 36.0 F (2.2 C) and wind Northeast at 9.2mph.'));
+        status.delay = 'true';
+        expect(subject.formatAirportStatus(status)).to.eq('There is currently a delay for Hartsfield-Jackson Atlanta International. The average delay time is 57 minutes. Delay is because of the following: AIRLINE REQUESTED DUE TO DE-ICING AT AIRPORT / DAL AND DAL SUBS ONLY. The current weather conditions are Light Snow, 36.0 F (2.2 C) and wind Northeast at 9.2mph.');
       });
     });
   });
